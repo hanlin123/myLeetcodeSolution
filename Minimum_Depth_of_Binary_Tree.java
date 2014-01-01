e27e666ad0f72d0b2ef66e37adf9ff131e57f0c3
hanlin123 <dandan526625@gmail.com>
2014-01-01 16:09:41 +0800

    'modify max depth of binary tree'

--- Minimum_Depth_of_Binary_Tree.java
+++ Minimum_Depth_of_Binary_Tree.java
@@ -10,16 +10,11 @@ public class TreeNode {
 public class Solution {
     public int minDepth(TreeNode root) {
         if(root==null)  return 0;
-        int leftResult = recursiveCheck(root.left);
-        int rightResult = recursiveCheck(root.right);
+        int leftResult = minDepth(root.left);
+        int rightResult = minDepth(root.right);
         
         if(leftResult==0)   return rightResult+1;
         if(rightResult==0)  return leftResult+1;
         return Math.min(leftResult, rightResult)+1;
-    }
-    
-    private static int recursiveCheck(TreeNode node){
-        if(node==null)  return 0;
-        return Math.min(recursiveCheck(node.left), recursiveCheck(node.right))+1;
     }
 }
